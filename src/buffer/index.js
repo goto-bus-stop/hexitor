02030da77ccf7df6460c143d25b092573785af7c
René Kooi <renee@kooi.me>
2017-01-13 19:32:49 +0100

    Reuse HexBuffer views.

--- src/buffer/index.js
+++ src/buffer/index.js
@@ -4,14 +4,33 @@ function createView (base, start, end) {
   view.start = start
   view.end = end
   view.equals = (other) =>
-    other.base === base && other.start === start && other.end === end
+    (view === other) ||
+    (other.base === base && other.start === start && other.end === end)
 
   return view
 }
 
 class HexBuffer extends Buffer {
+  constructor (...args) {
+    super(...args)
+    this.initHexBuffer()
+  }
+
+  initHexBuffer () {
+    this.viewsCache = {}
+  }
+
   view (start, end) {
-    return createView(this, start, end)
+    if (!this.viewsCache.hasOwnProperty(start)) {
+      this.viewsCache[start] = {}
+    } else if (this.viewsCache[start].hasOwnProperty(end)) {
+      return this.viewsCache[start][end]
+    }
+
+    const view = createView(this, start, end)
+
+    this.viewsCache[start][end] = view
+    return view
   }
 }
 
@@ -20,5 +39,6 @@ module.exports = HexBuffer
 HexBuffer.from = (arrayBuffer) => {
   const base = Buffer.from(arrayBuffer)
   Object.setPrototypeOf(base, HexBuffer.prototype)
+  base.initHexBuffer()
   return base
 }
