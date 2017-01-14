6db4f54e1ecb22c1340d4ae4a2c25252d1ca54e5
René Kooi <renee@kooi.me>
2017-01-14 13:38:18 +0100

    Inject state in Hotkey handlers.

--- src/components/hotkey.js
+++ src/components/hotkey.js
@@ -1,41 +1,50 @@
 const h = require('inferno-hyperscript')
+const Component = require('inferno-component')
 const { connect } = require('inferno-redux')
 const Mousetrap = require('mousetrap')
 
 const enhance = connect(
-  () => {},
-  dispatch => ({ dispatch })
+  (state) => ({}),
+  (dispatch) => ({ dispatch })
 )
+
+class Hotkey extends Component {
+  constructor (props, context) {
+    super(props, context)
+
+    this.store = context.store
+    this.handle = this.handle.bind(this)
+  }
+
+  handle (event, ...args) {
+    const { dispatch, action } = this.props
+
+    event.preventDefault()
+    dispatch(action(this.store.getState(), event, ...args))
+  }
+
+  componentDidMount () {
+    Mousetrap.bind(this.props.keys, this.handle)
+  }
+  componentWillUnmount () {
+    Mousetrap.unbind(this.props.keys)
+  }
+
+  render () {}
+}
 
 module.exports = enhance(Hotkey)
 module.exports.Hotkeys = enhance(Hotkeys)
 
-function Listener () {}
-
-function Hotkey (props) {
-  function handle (...args) {
-    props.dispatch(props.action(...args))
-  }
-
-  function bind () {
-    Mousetrap.bind(props.keys, handle)
-  }
-
-  function unbind () {
-    Mousetrap.unbind(props.keys)
-  }
-
-  return h(Listener, {
-    onComponentDidMount: bind,
-    onComponentWillUnmount: unbind
-  })
-}
-
 function Hotkeys (handlers) {
-  const dispatch = handlers.dispatch
+  const { dispatch } = handlers
 
   return h('span', Object.keys(handlers)
     .filter((keys) => keys !== 'dispatch')
-    .map((keys) => h(Hotkey, { dispatch, keys, action: handlers[keys] }))
+    .map((keys) => h(Hotkey, {
+      dispatch,
+      keys,
+      action: handlers[keys]
+    }))
   )
 }
